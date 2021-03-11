db061ed63f0a70884e3fec9f7fdd1a26cae97129
TheAus10 <49345932+TheAus10@users.noreply.github.com>
2021-03-11 10:49:45 -0600

    Add files via upload

---
+++ Merge and Insertion Sort/Merge Sort/mergeSort.h
@@ -0,0 +1,9 @@
+// Austin Reeves awr0047
+
+void mergeSort(double list[], int l, int h);
+	// takes in an array of numbers
+	// uses a recursive merge sort to sort the numbers
+
+void merge(double list[], int l, int m, int h);
+	// takes in an array, the upper and lower bound, and the middle number
+	// creates temp arrays to hold data until merged into initial array 
